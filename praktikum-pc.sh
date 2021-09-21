28491291ddcfe45e3deeae746bce561981ceb1b0
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2021-09-21 17:25:51 +0700

    Quote URLs

--- praktikum-pc.sh
+++ praktikum-pc.sh
@@ -8,10 +8,10 @@ pip install --quiet --upgrade scikit-image
 
 # Download sample file
 echo "Mendownload file contoh..."
-wget --quiet -O boneka.jpg https://docs.google.com/uc?export=download&id=1rGok7TVnZShF6sFNlTgDhZgEdMLlUH6x
-wget --quiet -O daun.png https://docs.google.com/uc?export=download&id=1SiTZGvMF2XXmHW4gPCLTz_KbFvzSArVP
-wget --quiet -O mobil.jpg https://docs.google.com/uc?export=download&id=1GS5uIT4gEZsZyOm3M1m9YIV3Z7f7-FcI
-wget --quiet -O sel.tif https://docs.google.com/uc?export=download&id=1EL8RO5wTgFV3MI6K0Pa00ZXg_fKR4VSX
+wget --quiet -O boneka.jpg "https://docs.google.com/uc?export=download&id=1rGok7TVnZShF6sFNlTgDhZgEdMLlUH6x"
+wget --quiet -O daun.png "https://docs.google.com/uc?export=download&id=1SiTZGvMF2XXmHW4gPCLTz_KbFvzSArVP"
+wget --quiet -O mobil.jpg "https://docs.google.com/uc?export=download&id=1GS5uIT4gEZsZyOm3M1m9YIV3Z7f7-FcI"
+wget --quiet -O sel.tif "https://docs.google.com/uc?export=download&id=1EL8RO5wTgFV3MI6K0Pa00ZXg_fKR4VSX"
 
 # Selesai
 echo "HAPPY CODING <3"
