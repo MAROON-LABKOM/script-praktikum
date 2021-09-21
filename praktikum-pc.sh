7560acc6e9eb70085c50b36d6205e02cf3ad5d67
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2021-09-21 19:18:19 +0700

    Add new asset for image segmentation

--- praktikum-pc.sh
+++ praktikum-pc.sh
@@ -12,6 +12,7 @@ wget --quiet -O boneka.jpg "https://docs.google.com/uc?export=download&id=1rGok7
 wget --quiet -O daun.png "https://docs.google.com/uc?export=download&id=1SiTZGvMF2XXmHW4gPCLTz_KbFvzSArVP"
 wget --quiet -O mobil.jpg "https://docs.google.com/uc?export=download&id=1GS5uIT4gEZsZyOm3M1m9YIV3Z7f7-FcI"
 wget --quiet -O sel.tif "https://docs.google.com/uc?export=download&id=1EL8RO5wTgFV3MI6K0Pa00ZXg_fKR4VSX"
+wget --quiet -O tulisan.jpg "https://docs.google.com/uc?export=download&id=1g6xOlK5XrvHgAJNdHRVHgCwi1YZ8puqX"
 
-# Selesai
+# Done
 echo "HAPPY CODING <3"
