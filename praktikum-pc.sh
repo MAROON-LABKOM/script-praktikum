ce366a21eef1d31657b81096b1a648abdf53779f
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2021-09-21 16:51:03 +0700

    Add script for computer vision

---
+++ praktikum-pc.sh
@@ -0,0 +1,8 @@
+# Update library
+pip install --upgrade skimage
+
+# Download sample file
+wget -o boneka.jpg https://docs.google.com/uc?export=download&id=1rGok7TVnZShF6sFNlTgDhZgEdMLlUH6x
+wget -o daun.png https://docs.google.com/uc?export=download&id=1SiTZGvMF2XXmHW4gPCLTz_KbFvzSArVP
+wget -o mobil.jpg https://docs.google.com/uc?export=download&id=1GS5uIT4gEZsZyOm3M1m9YIV3Z7f7-FcI
+wget -o sel.tif https://docs.google.com/uc?export=download&id=1EL8RO5wTgFV3MI6K0Pa00ZXg_fKR4VSX
