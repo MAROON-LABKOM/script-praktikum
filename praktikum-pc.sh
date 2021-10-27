ba26a2dc997148ad320f724bbeb73993a81727b4
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2021-10-27 20:02:31 +0700

    Remove figlet and use

--- praktikum-pc.sh
+++ praktikum-pc.sh
@@ -1,10 +1,13 @@
 # Hello
-sudo apt -qq install figlet
-figlet -f slant "MaroonLabkom"
+echo '    __  ___                             __          __    __                 '
+echo '   /  |/  /___ __________  ____  ____  / /   ____ _/ /_  / /______  ____ ___ '
+echo '  / /|_/ / __ `/ ___/ __ \/ __ \/ __ \/ /   / __ `/ __ \/ //_/ __ \/ __ `__ \'
+echo ' / /  / / /_/ / /  / /_/ / /_/ / / / / /___/ /_/ / /_/ / ,< / /_/ / / / / / /'
+echo '/_/  /_/\__,_/_/   \____/\____/_/ /_/_____/\__,_/_.___/_/|_|\____/_/ /_/ /_/ '
 
 # Update library
 echo "Menginstall library..."
-pip install --quiet --disable-pip-version-check --upgrade scikit-image
+pip install --quiet --use-feature=2020-resolver --disable-pip-version-check --upgrade scikit-image
 
 # Download sample file
 echo "Mendownload file contoh..."
