cf77f1ba26d4563dc1cd241e510155b229b196e9
kodeaqua <anajmizdn@gmail.com>
2022-03-16 19:52:03 +0700

    grafkom: create a line more easier

--- grafika-komputer/garis.cpp
+++ grafika-komputer/garis.cpp
@@ -1,34 +1,47 @@
 /*
-  Praktikum Struktur Data
+  Praktikum Grafika Komputer
   Laboratorium Komputer
   Universitas Pakuan
   2022
 */
 
 #include <gl/freeglut.h>
+#define WINDOW_WIDTH 360
+#define WINDOW_HEIGHT 360
+
+void createLine(float from_x, float from_y, float to_x, float to_y, float line_size)
+{
+  glLineWidth(line_size);
+  glBegin(GL_LINES);
+  glColor3f(1.0f, 1.0f, 1.0f);
+  glVertex2f(from_x, from_y);
+  glVertex2f(to_x, to_y);
+  glEnd();
+}
 
 void draw()
 {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+  glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
 
-    glLineWidth(2.0f);
-    glBegin(GL_LINES);
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glVertex2f(0.0f, 0.2f);
-    glVertex2f(0.0f, -0.2f);
-    glEnd();
+  createLine(-0.5f, -0.5f, 0.5f, 0.5f, 10.0f);
 
-    glFlush();
+  glFlush();
+}
+
+void window(int width, int height)
+{
+  glutReshapeWindow(WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 int main(int argc, char **argv)
 {
-    glutInit(&argc, argv);
-    glutInitWindowSize(480, 480);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-    glutCreateWindow("Grafkom LABKOM: Garis");
-    glutDisplayFunc(draw);
-    glutMainLoop();
-    return 0;
+  glutInit(&argc, argv);
+  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+  glutCreateWindow("Grafkom LABKOM: Garis");
+  glutReshapeFunc(window);
+  glutDisplayFunc(draw);
+  glutMainLoop();
+  return 0;
 }
