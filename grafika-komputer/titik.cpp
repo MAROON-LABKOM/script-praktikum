5e2b9d2ef09ae50a1506261c648de34da35ce8a3
kodeaqua <anajmizdn@gmail.com>
2022-03-16 19:44:10 +0700

    grafkom: create a point more easier

--- grafika-komputer/titik.cpp
+++ grafika-komputer/titik.cpp
@@ -1,32 +1,45 @@
 /*
-  Praktikum Struktur Data
+  Praktikum Grafika Komputer
   Laboratorium Komputer
   Universitas Pakuan
   2022
 */
 
 #include <gl/freeglut.h>
+#define WINDOW_WIDTH 360
+#define WINDOW_HEIGHT 360
+
+void createPoint(float x, float y, float point_size)
+{
+  glPointSize(point_size);
+  glBegin(GL_POINTS);
+  glColor3f(1.0f, 1.0f, 1.0f);
+  glVertex2f(x, y);
+  glEnd();
+}
 
 void draw()
 {
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glPointSize(5.0f);
-  glBegin(GL_POINTS);
-  glColor3f(1.0f, 1.0f, 1.0f);
-  glVertex2f(0.0f, 0.0f);
-  glEnd();
+  createPoint(0.0f, 0.0f, 10.0f);
 
   glFlush();
+}
+
+void window(int width, int height)
+{
+  glutReshapeWindow(WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 int main(int argc, char **argv)
 {
   glutInit(&argc, argv);
-  glutInitWindowSize(480, 480);
+  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
   glutCreateWindow("Grafkom LABKOM: Titik");
+  glutReshapeFunc(window);
   glutDisplayFunc(draw);
   glutMainLoop();
   return 0;
